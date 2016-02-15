dfb3fe3acbcdbc64fc6665d74c7bf3a62c5ec8f6
Denzel Ketter <D_ketter@yahoo.com>
2016-02-15 07:07:56 -0500

    added TabBarController

--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -42,7 +42,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UISearchBar
         
         
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let url = NSURL(string: "https://api.themoviedb.org/3/movie/\(endpoint)?api_key=\(apiKey)")
         let request = NSURLRequest(
             URL: url!,
             cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData,
